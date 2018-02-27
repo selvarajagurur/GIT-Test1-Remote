2c7548738a2360dd0b3e46c036216ec050e046df
SRG <selvarajaguru.r@gmail.com>
2018-02-27 14:08:17 -0500

    Second commit change

--- SRG_repo/src/main/java/com/testrepo/SRG_repo/testfunctions.java
+++ SRG_repo/src/main/java/com/testrepo/SRG_repo/testfunctions.java
@@ -3,5 +3,6 @@ package com.testrepo.SRG_repo;
 public class testfunctions {
 	public void test1(){
 	System.out.print("first commit in SRG_repo)");
+	System.out.println("second commit in SRG_repo");
 	}
 }
