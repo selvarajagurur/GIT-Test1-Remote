b792ee91d9dbf6faf9d547864c11ee58c032bdda
CRP <crpusany@gmail.com>
2018-02-27 16:06:49 -0500

    Third commit by CRP from remote repository

--- SRG_repo/src/main/java/com/testrepo/SRG_repo/testfunctions.java
+++ SRG_repo/src/main/java/com/testrepo/SRG_repo/testfunctions.java
@@ -4,5 +4,6 @@ public class testfunctions {
 	public void test1(){
 	System.out.print("first commit in SRG_repo)");
 	System.out.println("second commit in SRG_repo");
+	System.out.println("third commit in repo downloaded from remote_SRG_REPO");
 	}
 }
