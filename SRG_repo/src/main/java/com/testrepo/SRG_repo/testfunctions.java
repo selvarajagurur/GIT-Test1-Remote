148b2b1a19678a65308d6c039312912b43ef24c1
Guru Ramaswamy <crpusany@gmail.com>
2018-02-27 18:36:40 -0500

    sixth commit

--- SRG_repo/src/main/java/com/testrepo/SRG_repo/testfunctions.java
+++ SRG_repo/src/main/java/com/testrepo/SRG_repo/testfunctions.java
@@ -7,6 +7,7 @@ public class testfunctions {
 	System.out.println("third commit in repo downloaded from remote_SRG_REPO");
 	System.out.println("fourth commit in repo by GRS downloaded from remote_SRG_REPO");
 	System.out.println("fifth commit in repo by GRS downloaded from remote_SRG_REPO");
+	System.out.println("sixth commit in repo by GRS downloaded from remote_SRG_REPO");
 	
 	}
 }
