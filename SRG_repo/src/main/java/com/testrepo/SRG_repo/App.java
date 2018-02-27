bcc204f48ee7d4cd47eb58b22037689bb77cbda3
SRG <selvarajaguru.r@gmail.com>
2018-02-27 13:46:31 -0500

    First Commit from SRG

---
+++ SRG_repo/src/main/java/com/testrepo/SRG_repo/App.java
@@ -0,0 +1,13 @@
+package com.testrepo.SRG_repo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ SRG_repo/src/main/java/com/testrepo/SRG_repo/testfunctions.java
@@ -0,0 +1,7 @@
+package com.testrepo.SRG_repo;
+
+public class testfunctions {
+	public void test1(){
+	System.out.print("first commit in SRG_repo)");
+	}
+}
