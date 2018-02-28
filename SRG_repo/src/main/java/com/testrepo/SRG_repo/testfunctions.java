7364ec78edb0e235a72de4dd05c3957ceafdf0af
CRP <crpusany@gmail.com>
2018-02-28 10:27:58 -0500

    Second Commit from CRP_SecondDeveloper

--- SRG_repo/src/main/java/com/testrepo/SRG_repo/testfunctions.java
+++ SRG_repo/src/main/java/com/testrepo/SRG_repo/testfunctions.java
@@ -9,5 +9,7 @@ public class testfunctions {
 	System.out.println("fifth commit in repo by GRS downloaded from remote_SRG_REPO");
 	System.out.println("sixth commit in repo by GRS downloaded from remote_SRG_REPO");
 	
+	//---
+	System.out.println("Second Commit from CRP_SecondDeveloper");
 	}
 }
